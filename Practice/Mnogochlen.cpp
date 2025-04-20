5638f7c7a0e015c720037affd928203a52c86f80
кургуз никита <nikitakurguz2016@gmail.com>
2025-04-20 16:23:44 +0300

    Закончил с многочленами, осталось добавить проверку на вводимые данные

--- Practice/Mnogochlen.cpp
+++ Practice/Mnogochlen.cpp
@@ -10,18 +10,18 @@ int Input_n()
     while (1)
     {
         cout << "Введите степень многочлена (не более 20): "; cin >> n;
-        if (n > 0 && n <= 20) break;
+        if (n >= 0 && n <= 20) break;
     }
     return n;
 }
-double* Input_mnog(int n) 
+double* Input_mnog(int n)
 {
     double* kefs = (double*)malloc((n + 1) * sizeof(double));
     cout << "Введите коэффициенты многочлена, начиная со старшей степени:" << endl;
     for (int i = n; i >= 0; i--)
     {
         if (i > 1) cout << "Коэффициент при x^" << i << ": ";
-        if (i == 0) cout << "Свободный коэффициент: "; 
+        if (i == 0) cout << "Свободный коэффициент: ";
         if (i == 1) cout << "Коэффициент при x: ";
         cin >> kefs[i];
     }
@@ -46,7 +46,7 @@ void Output_mnog(double* kefs, int n)
         {
             if (a > 0) cout << " + ";
         }
-        if (a < 0) cout << "-";
+        if (a < 0) cout << " - ";
         if (fabs(a) != 1 || i == 0) cout << fabs(a);
 
         if (i > 1) cout << "x^" << i;
@@ -66,7 +66,7 @@ double* type1(int st1, int st2, double* kefs1, double* kefs2)
     for (int i = 0; i <= st2; i++) result_kefs[i] += kefs2[i];
     return result_kefs;
 }
-double* type2(int st1, int st2, double* kefs1, double* kefs2) 
+double* type2(int st1, int st2, double* kefs1, double* kefs2)
 {
     double* result = (double*)calloc(max(st1, st2) + 1, sizeof(double));
     for (int i = 0; i <= st1; i++) result[i] += kefs1[i];
@@ -75,66 +75,176 @@ double* type2(int st1, int st2, double* kefs1, double* kefs2)
 }
 double* type3(int st1, double* kefs1, double a)
 {
-    for (int i = st1; i << st1 >= 0; i--) kefs1[i] *= a;
+    for (int i = st1; i >= 0; i--) kefs1[i] *= a;
     return kefs1;
+}
+double* type4(int& st1, double* kefs1)
+{
+    if (st1 == 0)
+    {
+        double* n_kefs = (double*)malloc(sizeof(double));
+        n_kefs[0] = 0.0;
+        st1 = 0;
+        return n_kefs;
+    }
+    double* n_kefs = (double*)malloc(st1 * sizeof(double));
+    for (int i = 0; i < st1; ++i) n_kefs[i] = kefs1[i + 1] * (i + 1);
+    st1--;
+    return n_kefs;
+}
+
+Del_mnog type5(double* kefs1, int st1, double* kefs2, int st2)
+{
+    Del_mnog res;
+    bool is_zero_poly = true;
+    for (int i = 0; i <= st2; i++)
+    {
+        if (fabs(kefs2[i]) > 1e-10)
+        {
+            is_zero_poly = false;
+            break;
+        }
+    }
+
+    if (is_zero_poly)
+    {
+        cerr << "Ошибка: деление на нулевой многочлен!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    int real_st2 = st2;
+    while (real_st2 >= 0 && fabs(kefs2[real_st2]) < 1e-10) real_st2--;
+
+    if (real_st2 < 0)
+    {
+        cerr << "Ошибка: деление на нулевой многочлен!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (real_st2 == 0)
+    {
+        res.st_z = st1;
+        res.kefs_z = (double*)malloc((st1 + 1) * sizeof(double));
+        for (int i = 0; i <= st1; i++)
+            res.kefs_z[i] = kefs1[i] / kefs2[0];
+        res.st_ost = 0;
+        res.kefs_ost = (double*)malloc(sizeof(double));
+        res.kefs_ost[0] = 0.0;
+        return res;
+    }
+    if (real_st2 > st1)
+    {
+        res.st_z = 0;
+        res.kefs_z = (double*)calloc(1, sizeof(double));
+        res.st_ost = st1;
+        res.kefs_ost = (double*)malloc((st1 + 1) * sizeof(double));
+        for (int i = 0; i <= st1; i++) res.kefs_ost[i] = kefs1[i];
+        return res;
+    }
+
+    res.st_z = st1 - real_st2;
+    res.st_ost = real_st2 - 1;
+    res.kefs_z = (double*)calloc(res.st_z + 1, sizeof(double));
+    res.kefs_ost = (double*)malloc((st1 + 1) * sizeof(double));
+
+    for (int i = 0; i <= st1; i++) res.kefs_ost[i] = kefs1[i];
+    for (int i = res.st_z; i >= 0; i--)
+    {
+        if (fabs(res.kefs_ost[i + real_st2]) > 1e-10)
+        {
+            double coeff = res.kefs_ost[i + real_st2] / kefs2[real_st2];
+            res.kefs_z[i] = coeff;
+
+            for (int j = 0; j <= real_st2; j++)
+                res.kefs_ost[i + j] -= coeff * kefs2[j];
+        }
+    }
+    return res;
 }
 void run_calc()
 {
-	int n, st1, st2;
-    double* kefs1, *kefs2, *res;
-    int a;
-	cout << "Калькулятор многочленов." << endl;
-	while (1)
-	{
-		cout << "'b' - возврат в главное меню" << endl;
-		cout << "1 - сложение многочленов" << endl;
-		cout << "2 - вычитание многочленов" << endl;
-		cout << "3 - умножение многочлена на число" << endl;
-		cout << "4 - вычисление производной от многочлена" << endl;
-		cout << "5 - деление многочленов в столбик" << endl;
-		cout << "Выберите режим калькулятора: "; cin >> n;
-		if (n > 0 && n <= 5) break;
-	}
-    switch (n)
+    while (1)
     {
-    case 1:
-    {
-        st1 = Input_n();
-        kefs1 = Input_mnog(st1);
-        cout << "1 многочлен:" << endl;
-        Output_mnog(kefs1, st1);
-        st2 = Input_n();
-        kefs2 = Input_mnog(st2);
-        cout << "Второй многочлен:" << endl;
-        Output_mnog(kefs2, st2);
-        res = type1(st1, st2, kefs1, kefs2);
-        cout << "Результат после сложения: " << endl;
-        Output_mnog(res, max(st1, st2));
-    }; break;
-    case 2:
-    {
-        st1 = Input_n();
-        kefs1 = Input_mnog(st1);
-        cout << "1 многочлен:" << endl;
-        Output_mnog(kefs1, st1);
-        st2 = Input_n();
-        kefs2 = Input_mnog(st2);
-        cout << "Второй многочлен:" << endl;
-        Output_mnog(kefs2, st2);
-        res = type2(st1, st2, kefs1, kefs2);
-        cout << "Результат после вычитания: " << endl;
-        Output_mnog(res, max(st1, st2));
-    }; break;
-    case 3:
-    {
-        st1 = Input_n();
-        kefs1 = Input_mnog(st1);
-        cout << "1 многочлен:" << endl;
-        Output_mnog(kefs1, st1);
-        a = Input_a();
-        kefs2 = type3(st1, kefs1, a);
-        cout << "Результат после умножения многочлена на число: " << endl;
-        Output_mnog(kefs2, st1);
-    }; break;
+        int n, st1, st2;
+        double* kefs1, * kefs2, * res;
+        int a;
+        cout << "Калькулятор многочленов." << endl;
+        cout << "1 - сложение многочленов" << endl;
+        cout << "2 - вычитание многочленов" << endl;
+        cout << "3 - умножение многочлена на число" << endl;
+        cout << "4 - вычисление производной от многочлена" << endl;
+        cout << "5 - деление многочленов в столбик" << endl;
+        cout << "другая - возврат в главное меню" << endl;
+        cout << "Выберите режим калькулятора: "; cin >> n;
+        switch (n)
+        {
+        case 1:
+        {
+            st1 = Input_n();
+            kefs1 = Input_mnog(st1);
+            cout << "1 многочлен:" << endl;
+            Output_mnog(kefs1, st1);
+            st2 = Input_n();
+            kefs2 = Input_mnog(st2);
+            cout << "2 многочлен:" << endl;
+            Output_mnog(kefs2, st2);
+            res = type1(st1, st2, kefs1, kefs2);
+            cout << "Результат после сложения: " << endl;
+            Output_mnog(res, max(st1, st2));
+        }; break;
+        case 2:
+        {
+            st1 = Input_n();
+            kefs1 = Input_mnog(st1);
+            cout << "1 многочлен:" << endl;
+            Output_mnog(kefs1, st1);
+            st2 = Input_n();
+            kefs2 = Input_mnog(st2);
+            cout << "2 многочлен:" << endl;
+            Output_mnog(kefs2, st2);
+            res = type2(st1, st2, kefs1, kefs2);
+            cout << "Результат после вычитания: " << endl;
+            Output_mnog(res, max(st1, st2));
+        }; break;
+        case 3:
+        {
+            st1 = Input_n();
+            kefs1 = Input_mnog(st1);
+            cout << "Ваш многочлен:" << endl;
+            Output_mnog(kefs1, st1);
+            a = Input_a();
+            kefs2 = type3(st1, kefs1, a);
+            cout << "Результат после умножения многочлена на число: " << endl;
+            Output_mnog(kefs2, st1);
+        }; break;
+        case 4:
+        {
+            st1 = Input_n();
+            kefs1 = Input_mnog(st1);
+            cout << "Ваш многочлен:" << endl;
+            Output_mnog(kefs1, st1);
+            kefs2 = type4(st1, kefs1);
+            cout << "Производная многочлена: " << endl;
+            Output_mnog(kefs2, st1);
+        }; break;
+        case 5:
+        {
+            st1 = Input_n();
+            kefs1 = Input_mnog(st1);
+            cout << "1 многочлен:" << endl;
+            Output_mnog(kefs1, st1);
+            st2 = Input_n();
+            kefs2 = Input_mnog(st2);
+            cout << "2 многочлен:" << endl;
+            Output_mnog(kefs2, st2);
+            Del_mnog Data = type5(kefs1, st1, kefs2, st2);
+            cout << "Целая часть: ";
+            Output_mnog(Data.kefs_z, Data.st_z);
+            cout << "Остаток: ";
+            Output_mnog(Data.kefs_ost, Data.st_ost);
+        }; break;
+        default: return; break;
+        }
+        cout << "\n----------------------------------------------------\n";
     }
+
 }
--- Practice/mnogochleni.h
+++ Practice/mnogochleni.h
@@ -1,5 +1,12 @@
 ﻿#ifndef MNOGOCHLENI_H
 #define MNOGOCHLENI_H
+struct Del_mnog
+{
+    double* kefs_z;
+    double* kefs_ost;
+    int st_z;
+    int st_ost;
+};
 void run_calc();
 int Input_n();
 double Input_a();
@@ -8,4 +15,6 @@ double* Input_mnog(int n);
 double* type1(int st1, int st2, double* kefs1, double* kefs2);
 double* type2(int st1, int st2, double* kefs1, double* kefs2);
 double* type3(int st1, double* kefs1, double a);
+double* type4(int& st1, double* kefs1);
+Del_mnog type5(double* kefs1, int st1, double* kefs2, int st2);
 #endif //MNOGOCHLENI_H
